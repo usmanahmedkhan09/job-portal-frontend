eb5abdbbe790829a11b08b1c7f8f8483827810da
usmanahmedkhan09 <usmanahmedkhan@live.com>
2025-02-26 18:55:49 +0500

    change import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
